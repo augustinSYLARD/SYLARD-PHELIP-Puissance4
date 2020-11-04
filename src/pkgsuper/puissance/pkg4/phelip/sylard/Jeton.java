6a4ccc19f9769342d478c1e573599206d74652f8
augus <augus@DESKTOP-QK32DV4>
2020-11-04 16:22:01 +0100

    restauration du projet V 1.0

--- src/pkgsuper/puissance/pkg4/phelip/sylard/Jeton.java
+++ src/pkgsuper/puissance/pkg4/phelip/sylard/Jeton.java
@@ -12,10 +12,9 @@ package pkgsuper.puissance.pkg4.phelip.sylard;
 public class Jeton {
 
     String Couleur;//contient la couleur du jeton
-    int a;
+    
     public Jeton (String couleur) {//constructeur de la class Jeton qui donne au jeton la couleur donner en parametre
         Couleur=couleur;
-        a=1;
     }
     public String lireCouleur() {//cette fonction permet de lire la couleur d'un jeton
         return Couleur;
